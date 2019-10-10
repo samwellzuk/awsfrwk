9150ecde7ee0befb21cfb460f253ebad3ac74b4d
Samwell Zuk <samwell.zuk@outlook.com>
2019-10-10 15:24:04 +0800

    update runtime to python3.7

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # awsfrwk
+
+以下函数通过装饰器封装对被装饰函数的并发调用。
+通过函数的taskid/funcid等信息可以达到对每个函数并发控制的目的。即：相同 taskid-funcid-awsfunc 作为一个并发调用的单元。
+内部会根据这个来自动建立对应的aws lambda函数，aws sqs队列
--- awsfunc.py
+++ awsfunc.py
@@ -5,12 +5,6 @@ from functools import wraps, partial
 from .reflect import check_func
 from .awscall import call_by_aws
 
-"""
-以下函数通过装饰器封装对被装饰函数的并发调用。
-通过函数的taskid/funcid等信息可以达到对每个函数并发控制的目的。即：相同 taskid-funcid-awsfunc 作为一个并发调用的单元。
-内部会根据这个来自动建立对应的aws lambda函数，aws sqs队列
-"""
-
 
 def aws_common(func=None, *, concurrent=None, func_id=None, enabled=True, memory=128, maxretry=1):
     if func is None:
